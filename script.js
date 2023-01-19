a1019b1ffb322713da2575db2032340cf7547d9a
Ibimina <ibimina.c.hart@gmail.com>
2023-01-19 16:30:42 +0100

    docs: select nav element and set attrbute

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var button = document.querySelector("button");
+var nav = document.querySelector("nav");
 var toggleMenu = function () {
-    var visible = button.getAttribute("aria-expanded");
+    var visible = nav.getAttribute("data-visible");
     if (visible === "false") {
+        nav.setAttribute("data-visible", "true");
         button.setAttribute("aria-expanded", "true");
         button.setAttribute("aria-label", "close");
     }
     else {
+        nav.setAttribute("data-visible", "false");
         button.setAttribute("aria-expanded", "false");
         button.setAttribute("aria-label", "open");
     }
--- script.ts
+++ script.ts
@@ -1,11 +1,14 @@
 const button = document.querySelector("button")!
+const nav = document.querySelector("nav")!
 
 const toggleMenu = () => {
-    let visible = button.getAttribute("aria-expanded")
+    let visible = nav.getAttribute("data-visible")
     if (visible === "false") {
+        nav.setAttribute("data-visible", "true")
         button.setAttribute("aria-expanded", "true")
         button.setAttribute("aria-label", "close")
     } else {
+        nav.setAttribute("data-visible", "false")
         button.setAttribute("aria-expanded", "false")
         button.setAttribute("aria-label", "open")
     }
