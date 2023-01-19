11697b28e89cd36dc064f95e76f79f2200205572
Ibimina <ibimina.c.hart@gmail.com>
2023-01-19 15:34:40 +0100

    docs: added a funtion to control nav menu toggle

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+var button = document.querySelector("button");
+var toggleMenu = function () {
+    var visible = button.getAttribute("aria-expanded");
+    if (visible === "false") {
+        button.setAttribute("aria-expanded", "true");
+        button.setAttribute("aria-label", "close");
+    }
+    else {
+        button.setAttribute("aria-expanded", "false");
+        button.setAttribute("aria-label", "open");
+    }
+};
+button.addEventListener("click", toggleMenu);
--- script.ts
+++ script.ts
@@ -1,1 +1,13 @@
-console.log("hello")
+const button = document.querySelector("button")!
+
+const toggleMenu = () => {
+    let visible = button.getAttribute("aria-expanded")
+    if (visible === "false") {
+        button.setAttribute("aria-expanded", "true")
+        button.setAttribute("aria-label", "close")
+    } else {
+        button.setAttribute("aria-expanded", "false")
+        button.setAttribute("aria-label", "open")
+    }
+}
+button.addEventListener("click", toggleMenu)
